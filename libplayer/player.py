eaafe3c401b3d8351c2da62a678ad86624f44482
Jorde, Andreas <andreas.jorde@nuance.com>
2018-06-04 16:40:22 -0400

    First commit

---
+++ libplayer/player.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import sys
+from libplayer.channel import *
+from libplayer.utils import *
+
+# Get the plugin url in plugin:// notation.
+__url__ = sys.argv[0]
+# Get the plugin handle as an integer number.
+__handle__ = int(sys.argv[1])
+
+__context__ = None
+
+class Hr3Player(xbmc.Player):
+    def __init__(self):
+        print 'Hr3Player initialized'
+        xbmc.Player.__init__(self)
+    
+    def onPlayBackError(self):
+        logStr = 'onPlayBackError called for Hr3Player'
+        print logStr
+        xbmc.log(logStr, xbmc.LOGERROR)
+    
+def list_shows(context):
+    """
+    Create the list of video categories in the Kodi interface.
+    :return: None
+    """
+    # Get video categories
+    shows = context['showList']
+    # Create a list for our items.
+    listing = []
+    # Iterate through categories
+    index = 0
+    for show in shows:
+        if show['active']:
+            # Create a list item with a text label and a thumbnail image.
+            list_item = xbmcgui.ListItem(label=show['name'], thumbnailImage=show['image'])
+ 
+            # Create a URL for the plugin recursive callback.
+            # Example: plugin://plugin.video.example/?action=listing&category=Animals
+            is_folder = True
+            if index == 0:
+                is_folder = False
+            url = '{0}?action=listing&show={1}'.format(__url__, str(index))
+            # is_folder = True means that this item opens a sub-list of lower level items.
+
+            # Add our item to the listing as a 3-element tuple.
+            listing.append((url, list_item, is_folder))
+        index += 1
+    # Add our listing to Kodi.
+    # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
+    # instead of adding one by ove via addDirectoryItem.
+    xbmcplugin.addDirectoryItems(__handle__, listing, len(listing))
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    #xbmcplugin.addSortMethod(__handle__, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(__handle__)
+    
+def list_episodes(context, index):
+    # Get video categories
+    showid = context['showList']
+    episodes = context['episodes']
+    # Create a list for our items.
+    listing = []
+    # Iterate through categories
+    index = 0
+    for episode in episodes:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=episode['title'], label2='Test Label 2', thumbnailImage=episode['image'])
+        list_item.setInfo('video', 'Test Info')
+        url = '{0}?action=play&url={1}&index={2}'.format(__url__, episode['link'], index)
+        is_folder = False
+        listing.append((url, list_item, is_folder))
+        index += 1
+    xbmcplugin.addDirectoryItems(__handle__, listing, len(listing))
+    xbmcplugin.endOfDirectory(__handle__)
+    
+def dispatch(url, handle, parameter):
+    logStr = "Entry: " + url + ", " + str(handle) + ", " + parameter
+    xbmc.log(logStr, xbmc.LOGINFO)
+    parameters = extractParameters(parameter)
+    action = None
+    show = None
+    url = None
+    if parameters != None:
+        for action in parameters:
+            logStr = action + " = " + parameters[action]
+            xbmc.log(logStr, xbmc.LOGINFO)
+        if 'action' in parameters:
+            action = parameters['action']
+        if 'show' in parameters:
+            show = int(parameters['show'])
+        if 'url' in parameters:
+            url = parameters['url']
+    else:
+        xbmc.log("No actions", xbmc.LOGINFO)
+        action = None   
+        
+    context = ChannelContext(xbmcaddon.Addon())
+
+    if action == None:
+        list_shows(context)
+    elif action == 'listing':
+        loader = ChannelLoader()
+        loader.loadEpisodeList(context, show)
+        # Play live stream immediately
+        if show == 0:
+            video = context['episodes'][0]['link']
+            resolved_video = loader.resolveLiveUrl(video)
+            listitem = xbmcgui.ListItem('Ironman')
+            listitem.setInfo('video', {'Title': '', 'Genre': ''})
+            player = Hr3Player()
+            player.play(resolved_video, listitem)
+        else:
+            list_episodes(context, show)
+    elif action == 'play':
+        video = getVideoLink(url)
+        listitem = xbmcgui.ListItem('Ironman')
+        listitem.setInfo('video', {'Title': 'Title', 'Genre': 'Genre'})
+        Hr3Player().play(video, listitem)
